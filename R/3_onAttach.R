38fefafc0f8b2eaf0cd05ceca8dd396576fd48e5
Richard Aubrey White <RichardAubrey.White@fhi.no>
2022-02-09 07:47:32 +0100

    update description and onattach

--- R/3_onAttach.R
+++ R/3_onAttach.R
@@ -1,9 +1,15 @@
 #' @import data.table ggplot2
 #' @importFrom magrittr %>%
 .onAttach <- function(libname, pkgname) {
+  version <- tryCatch(
+    utils::packageDescription("splshiny", fields = "Version"),
+    warning = function(w){
+      1
+    }
+  )
   packageStartupMessage(paste0(
     "splshiny ",
-    utils::packageDescription("splshiny")$Version,
+    version,
     "\n",
     "https://docs.sykdomspulsen.no/splshiny"
   ))
